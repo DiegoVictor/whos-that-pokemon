fb653a6333a633f0af01780f18ba5b473061496c
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-08-23 22:07:20 -0300

    Create serverless project

---
+++ serverless.ts
@@ -0,0 +1,29 @@
+import type { AWS } from "@serverless/typescript";
+
+const serverlessConfiguration: AWS = {
+  service: "whos-that-pokemon",
+  frameworkVersion: "2",
+  custom: {
+    webpack: {
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
+  },
+  plugins: [
+    "serverless-webpack",
+  ],
+  provider: {
+    name: "aws",
+    runtime: "nodejs14.x",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+    },
+    lambdaHashingVersion: "20201221",
+  },
+};
+
+module.exports = serverlessConfiguration;
