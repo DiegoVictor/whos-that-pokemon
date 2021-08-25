edd54934081fd175d229b8f46e14ee589f81b007
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-08-25 00:04:35 -0300

    Configure lambda handler and event

---
+++ src/functions/recognize/index.ts
@@ -0,0 +1,19 @@
+import { handlerPath } from "@libs/handlerResolver";
+import schema from "./schema";
+
+export default {
+  handler: `${handlerPath(__dirname)}/handler.recognize`,
+  events: [
+    {
+      http: {
+        method: "post",
+        path: "recognize",
+        request: {
+          schema: {
+            "application/json": schema,
+          },
+        },
+      },
+    },
+  ],
+};
