aa87be76ef3465a018e523c913158061c37ec4c3
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-28 19:09:37 -0300

    Fix bucket name environment variable

--- src/functions/recognize/index.ts
+++ src/functions/recognize/index.ts
@@ -3,6 +3,7 @@ import { handlerPath } from "@utils/handlerResolver";
 export default {
   handler: `${handlerPath(__dirname)}/handler.recognize`,
   environment: {
+    BUCKET_NAME: "${self:custom.bucketName}",
     PROJECT_VERSION_ARN_SECRET_NAME:
       "${self:custom.projectVersionArnSecretName}",
   },
