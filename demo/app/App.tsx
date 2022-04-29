b833f0c1b1b2e2a2401821c7ee93144dac3eebb5
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-29 17:05:42 -0300

    Update camera usage

--- demo/app/App.tsx
+++ demo/app/App.tsx
@@ -42,7 +42,7 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
+      const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === "granted");
     })();
   }, []);
