806e0a51c9b35ad63108e3213127fab546a6b3d5
Diego Victor <diegovictorgonzaga@gmail.com>
2022-05-01 00:42:21 -0300

    Update serverless.ts

--- serverless.ts
+++ serverless.ts
@@ -1,51 +1,52 @@
-import type { AWS } from "@serverless/typescript";
+import type { AWS } from '@serverless/typescript';
 
-import WhosThatPokemonRecognize from "@functions/recognize";
+import WhosThatPokemonRecognize from '@functions/recognize';
 
 const serverlessConfiguration: AWS = {
-  service: "whos-that-pokemon",
-  frameworkVersion: "3",
+  service: 'whos-that-pokemon',
+  frameworkVersion: '3',
   useDotenv: true,
   custom: {
     webpack: {
-      webpackConfig: "./webpack.config.js",
+      webpackConfig: './webpack.config.js',
       includeModules: true,
     },
-    projectName: "${file(./variables.json):projectName}",
-    projectVersionName: "${file(./variables.json):projectVersionName}",
-    projectVersionArnSecretName: "projectVersionArn",
-    bucketName: "${file(./variables.json):bucket}",
+    projectName: '${file(./variables.json):ProjectName}',
+    projectVersionName: '${file(./variables.json):ProjectVersionName}',
+    projectVersionArnSecretName:
+      '${file(./variables.json):ProjectVersionArnSecretName}',
+    bucketName: '${file(./variables.json):Bucket}',
   },
-  plugins: ["serverless-webpack", "serverless-offline"],
+  plugins: ['serverless-webpack', 'serverless-offline'],
   provider: {
-    name: "aws",
-    region: "us-east-1",
-    runtime: "nodejs14.x",
+    name: 'aws',
+    region: 'us-east-1',
+    runtime: 'nodejs14.x',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
     },
     iam: {
       role: {
         statements: [
           {
-            Effect: "Allow",
-            Action: ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
-            Resource: "arn:aws:s3:::*",
+            Effect: 'Allow',
+            Action: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
+            Resource: 'arn:aws:s3:::*',
           },
           {
-            Effect: "Allow",
-            Action: ["rekognition:DetectCustomLabels"],
-            Resource: "*",
+            Effect: 'Allow',
+            Action: ['rekognition:DetectCustomLabels'],
+            Resource: '*',
           },
           {
-            Effect: "Allow",
-            Action: ["secretsmanager:GetSecretValue"],
+            Effect: 'Allow',
+            Action: ['secretsmanager:GetSecretValue'],
             Resource: {
-              Ref: "WhosThatPokemonProjectVersionArnSecret",
+              Ref: 'WhosThatPokemonProjectVersionArnSecret',
             },
           },
         ],
@@ -56,22 +57,22 @@ const serverlessConfiguration: AWS = {
   resources: {
     Resources: {
       WhosThatPokemonBucket: {
-        Type: "AWS::S3::Bucket",
+        Type: 'AWS::S3::Bucket',
         Properties: {
-          BucketName: "${self:custom.bucketName}",
+          BucketName: '${self:custom.bucketName}',
         },
       },
       WhosThatPokemonProject: {
-        Type: "AWS::Rekognition::Project",
+        Type: 'AWS::Rekognition::Project',
         Properties: {
-          ProjectName: "${self:custom.projectName}",
+          ProjectName: '${self:custom.projectName}',
         },
       },
       WhosThatPokemonProjectVersionArnSecret: {
-        Type: "AWS::SecretsManager::Secret",
+        Type: 'AWS::SecretsManager::Secret',
         Properties: {
-          Description: "Rekognition Project Version Arn",
-          Name: "${self:custom.projectVersionArnSecretName}",
+          Description: 'Rekognition Project Version Arn',
+          Name: '${self:custom.projectVersionArnSecretName}',
           SecretString: '{"ProjectVersionArn":""}',
         },
       },
